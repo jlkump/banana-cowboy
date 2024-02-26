89d72e726087ff2c9d834648aa18d057e369719b
Joaquin <jake.niles654@utexas.edu>
2024-02-26 17:02:59 -0600

    added improved cel shader with support for additional lighting

--- Assets/Materials/Shaders/CustomNodes/CelShadingLibrary.hlsl
+++ Assets/Materials/Shaders/CustomNodes/CelShadingLibrary.hlsl
@@ -3,18 +3,38 @@
 
 #ifndef SHADERGRAPH_PREVIEW
 
+// shading is based on Robin Seibold's implementation of cel shading since
+// his explanation of handling multiple lights was digestible for my dumb ass
+// which is ALSO based on Roystans toon shader linked here https://roystan.net/articles/toon-shader/
+
+// for smoothstepping the edges of each component of the cel shading
+// so it doesnt look as harsh
+struct ThresholdConstants {
+    float diffuse;
+    float specular;
+    float rim;
+    float distanceAttenuation;
+    float shadowAttenuation;
+};
+
 // It was suggested I do this to keep things organized and (easier) to understand and I like it
-struct ShadingVariables {
+struct SurfaceValues {
+    float4 ambient;
+    float4 albedo;
     float3 normal;
     float3 viewDir;
     float smoothness;
     float shininess;
+    float rimStrength;
+    float rimAmount;
     float rimThreshold;
+    ThresholdConstants tc;
 };
 
-float3 CalculateCelShading(Light l, ShadingVariables s) {
+float3 CalculateCelShading(Light l, SurfaceValues s) {
     // shadow attenuation
-    float attenuation = l.shadowAttenuation * l.distanceAttenuation;
+    float attenuation = smoothstep(0.0f, s.tc.shadowAttenuation, l.shadowAttenuation) * 
+        smoothstep(0.0f, s.tc.distanceAttenuation, l.distanceAttenuation);
     // diffuse calculations
     float diffuse = saturate(dot(s.normal, l.direction));
     diffuse *= attenuation;
@@ -25,23 +45,45 @@ float3 CalculateCelShading(Light l, ShadingVariables s) {
     specular *= diffuse;
     // rim lighting
     float rim = 1 - dot(s.viewDir, s.normal);
-    rim *= pow(diffuse, s.rimThreshold);
+    rim *= diffuse;
+    // smoothstep everything here after all calcs are done to avoid weird lighting stuff
+    diffuse = smoothstep(0.0f, s.tc.diffuse, diffuse);
+    specular = s.smoothness * smoothstep(0.005f, 0.005f + s.tc.specular * s.smoothness, specular);
+    rim = s.rimStrength * smoothstep( s.rimAmount - 0.5f * s.tc.rim, 
+      s.rimAmount + 0.5f * s.tc.rim, rim );
     // final color output
+    //return rim;
+    // return s.albedo * l.color * (diffuse + s.ambient + max(specular, rim));
     return l.color * (diffuse + max(specular, rim));
 }
 #endif
 
-void LightingCelShaded_float(float3 Normal, float3 View, float Smoothness, float RimThreshold, float3 Position, out float3 Col) {
+void LightingCelShaded_float(float3 Normal, float3 View, float Smoothness, float RimStrength, float RimAmount, float RimThreshold,
+      float3 Position, float EdgeDiffuse, float EdgeSpecular, float EdgeRim, float EdgeDistanceAttenuation, 
+      float EdgeShadowAttenuation, float4 Ambient, float4 Albedo, out float3 Col) {
     #if defined(SHADERGRAPH_PREVIEW)
         Col = half3(0.5f, 0.5f, 0.5f);
     #else
-        // populate our shading variables
-        ShadingVariables s;
+        //Col = Albedo;
+        // populate our surface shading variables
+        SurfaceValues s;
+        s.ambient = Ambient;
+        s.albedo = Albedo;
         s.normal = normalize(Normal);
         s.viewDir = normalize(View);
         s.smoothness = Smoothness;
         s.shininess = exp2(10 * Smoothness + 1);
+        s.rimStrength = RimStrength;
+        s.rimAmount = RimAmount;
         s.rimThreshold = RimThreshold;
+        // populate thresholding constants for shading
+        ThresholdConstants tcon;
+        tcon.diffuse = EdgeDiffuse;
+        tcon.specular = EdgeSpecular;
+        tcon.rim = EdgeRim;
+        tcon.distanceAttenuation = EdgeDistanceAttenuation;
+        tcon.shadowAttenuation = EdgeShadowAttenuation;
+        s.tc = tcon;
         // shadow stuff
         #if SHADOWS_SCREEN
             float4 clipPos = TransformWorldToHClip(Position);
@@ -58,6 +100,9 @@ void LightingCelShaded_float(float3 Normal, float3 View, float Smoothness, float
             light = GetAdditionalLight(i, Position, 1);
             Col += CalculateCelShading(light, s);
         }
+        // do final lighting calculations
+        Col += Ambient;
+        Col *= Albedo;
     #endif
 }
 
